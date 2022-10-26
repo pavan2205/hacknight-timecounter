263fb15357bacbf73cd81a736a4b9ff6dfa412d1
Pavan <pavannaik2205@gmail.com>
2022-10-26 17:38:19 +0530

    created timecounter

---
+++ script.js
@@ -0,0 +1,78 @@
+const countToDate = new Date("October 26, 2022 16:34:00").getTime();
+let previousTimeBetweenDates;
+let countdownTimer = () => {
+  const currentDate = new Date();
+  const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
+  flipAllCards(timeBetweenDates);
+
+  previousTimeBetweenDates = timeBetweenDates;
+};
+
+function flipAllCards(time) {
+  const seconds = time % 60;
+  const minutes = Math.floor(time / 60) % 60;
+  const hours = Math.floor(time / 3600) % 24;
+  const days = Math.floor(time / (3600 * 24));
+
+  flip(document.querySelector("[data-days-tens]"), Math.floor(days / 10));
+  flip(document.querySelector("[data-days-ones]"), days % 10);
+  flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10));
+  flip(document.querySelector("[data-hours-ones]"), hours % 10);
+  flip(document.querySelector("[data-minutes-tens]"), Math.floor(minutes / 10));
+  flip(document.querySelector("[data-minutes-ones]"), minutes % 10);
+  flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10));
+  flip(document.querySelector("[data-seconds-ones]"), seconds % 10);
+
+  if (days == 0 && hours == 0 && minutes == 0 && seconds == 0) {
+    clearInterval(countdown);
+    let fram = setInterval(frame, 500);
+  }
+}
+
+function frame() {
+  confetti({
+    particleCount: 100,
+    angle: 60,
+    spread: 80,
+    origin: { x: 0 },
+    // colors: colors,
+  });
+  confetti({
+    particleCount: 100,
+    angle: 120,
+    spread: 80,
+    origin: { x: 1 },
+    // colors: colors,
+  });
+}
+
+function flip(flipCard, newNumber) {
+  const topHalf = flipCard.querySelector(".top");
+  const startNumber = parseInt(topHalf.textContent);
+  if (newNumber === startNumber) return;
+
+  const bottomHalf = flipCard.querySelector(".bottom");
+  const topFlip = document.createElement("div");
+  topFlip.classList.add("top-flip");
+  const bottomFlip = document.createElement("div");
+  bottomFlip.classList.add("bottom-flip");
+
+  top.textContent = startNumber;
+  bottomHalf.textContent = startNumber;
+  topFlip.textContent = startNumber;
+  bottomFlip.textContent = newNumber;
+
+  topFlip.addEventListener("animationstart", (e) => {
+    topHalf.textContent = newNumber;
+  });
+  topFlip.addEventListener("animationend", (e) => {
+    topFlip.remove();
+  });
+  bottomFlip.addEventListener("animationend", (e) => {
+    bottomHalf.textContent = newNumber;
+    bottomFlip.remove();
+  });
+  flipCard.append(topFlip, bottomFlip);
+}
+
+let countdown = setInterval(countdownTimer);
